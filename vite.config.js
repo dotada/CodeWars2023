9c1ea5a131aa7f6877f24f67656e4bcb8bbbdffb
dotada <luboslav.blazhev@gmail.com>
2023-12-01 13:24:22 +0200

    yes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../dist'
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
   },
   server: {
-    port: 8080
+    port: 8080,
+    hot: true
   }
 }
